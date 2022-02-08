385dd30a11e67407ee4aa1c967dadd6c411b8a0e
Quentin Glorieux <quentin@rubidiumweb.fr>
2022-02-08 19:29:24 +0100

    shorter syntax

--- components/ResearchItem.vue
+++ components/ResearchItem.vue
@@ -3,11 +3,13 @@
     <div  class="flex m-10 text-gray-400 ">
         <div>
             <div class=" text-gray-300 shadow-xs text-2xl mb-10"> 
-            {{ title }}
+            {{ science_object.title }}
+            <!-- {{ title }} : if the prop is more specific see ResearchItem2.vue-->
             </div>
-            {{ content }}
+            {{ science_object.content }}
+            <!-- {{ content }} : if the prop is more specific -->
             </div>
-            <nuxt-img class=" object-contain w-200 ml-10 saturate-0" :src="image_name" 
+            <nuxt-img class=" object-contain w-200 ml-10 saturate-0" :src="science_object.image_name" 
             width="200"
             />
     </div>
@@ -17,10 +19,14 @@
 <script>
 export default {
   props: {
-      title: '',
-      content: '',
-      image_name: '',
-  }
+      science_object: '',
+  },
+// if the prop is more specific see ResearchItem2.vue-->
+//     props: {
+//       title: '',
+//       content: '',
+//       image_name: '',
+//   }
 }
 </script>
 
