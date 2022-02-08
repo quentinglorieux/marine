9dbf5ae978eefd956671fc8d7357c34496c92e3f
Quentin Glorieux <quentin@rubidiumweb.fr>
2022-02-08 20:38:35 +0100

    publication v1

---
+++ components/PublicationItem.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+    <div  class="flex m-10 text-gray-400 ">
+
+            <div class=" text-gray-300 shadow-xs text-2xl mb-10"> 
+            {{ publication.acf.publication_title }}
+            </div>
+            <div>
+            {{ publication.acf.publication_journal }} -   {{ publication.acf.publication_dop }}
+            </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+      publication:'',
+  }
+}
+</script>
+
