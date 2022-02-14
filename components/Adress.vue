996413e26b285f2f7dfd8576cad90b9072c2442c
Quentin Glorieux <quentin@rubidiumweb.fr>
2022-02-14 20:01:24 +0100

    API coding address

--- components/Adress.vue
+++ components/Adress.vue
@@ -1,11 +1,35 @@
 <template>
 <div class="flex flex-col text-gray-300 content-end my-20 mr-10 text-right">
-    <div > CRNL, Impact Team, Bat. Inserm, </div>
-    <div > 16 Avenue du Doyen Jean Lépine </div>
-    <div > 69500 Bron </div>
-    <div > marine.vernet@inserm.fr</div>
+    <div > {{address.ligne1}}, </div>
+    <div > {{address.ligne2}}. </div>
+    <div > {{options.acf.contactinfo_lab_zip}} {{options.acf.contactinfo_lab_city}} </div>
+    <div > {{options.acf.contactinfo_email}}</div>
 </div>
 </template>
+
+
+
+<script>
+  export default {
+    data() {
+      return {
+        address: {
+          ligne1: 'CRNL, Impact Team, Bat. Inserm',
+          ligne2: '16 Avenue du Doyen Jean Lépine'
+        },
+        options: '',
+      }
+    },
+    async fetch() {
+      // This could also be an action dispatch
+      this.options = await fetch(
+        'http://marinevernet.fr/wp-json/acf/v3/options/options'
+      ).then(res => res.json())
+    },
+
+  }
+</script>
+
 
 <style scoped>
 p span {
--- components/Intro.vue
+++ components/Intro.vue
@@ -3,14 +3,10 @@
     <div  class="flex m-10 text-gray-400 ">
         <div>
             <div class=" text-gray-300 shadow-xs text-2xl mb-10"> 
-            Welcome to the {{  planet }}
+            Welcome to the {{  options.acf.siteinfo_site_tag_line }}
             </div>
-            <!-- <div v-html="options.acf.siteinfo_description"> -->
-            <!-- </div> -->
-            <!-- <p class="mt-3">To explore these questions, we use a combination of methods including psychophysics, eye movements tracking,
-            transcranial magnetic stimulation, functional magnetic resonance imaging, 
-            electro- and magnetoencephalography.
-            </p> -->
+            <div v-html="options.acf.siteinfo_description"> </div>
+            
             <p v-if="$fetchState.pending">Fetching mountains...</p>
             <p v-else-if="$fetchState.error">An error occurred :(</p>
             
@@ -18,7 +14,6 @@
             <nuxt-img class="saturate-0 object-contain" src="/Picture_MV_Brain.jpg" 
             width="300"
             />
-             -- {{ options }}
     </div>
 </div>
 </template>
@@ -28,8 +23,8 @@
   export default {
     data() {
       return {
-        options: [],
-        planet: ''
+        options: {},
+       // planet: {}
       }
     },
     async fetch() {
@@ -38,17 +33,17 @@
         'http://marinevernet.fr/wp-json/acf/v3/options/options'
       ).then(res => res.json())
     },
-    async asyncData() {
-        const planet = await fetch(
-            'http://marinevernet.fr/wp-json/acf/v3/options/options'
-        ).then((res) => {
-            if (res.ok) {
-            return res.json()
-            }
-            throw new Error(res.status)
-        })
-        return { planet }
-    },
+    // async asyncData() {
+    //     const planet = await fetch(
+    //         'http://marinevernet.fr/wp-json/acf/v3/options/options'
+    //     ).then((res) => {
+    //         if (res.ok) {
+    //         return res.json()
+    //         }
+    //         throw new Error(res.status)
+    //     })
+    //     return { planet }
+    // },
 
   }
 </script>
